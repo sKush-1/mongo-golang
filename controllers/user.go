2a3ba315b1097a92c81f44bf2247f7b06ed2d38c
sKush-1 <kushsumit27@gmail.com>
2025-08-24 20:30:45 +0530

    use new mongo package

--- controllers/user.go
+++ controllers/user.go
@@ -1,58 +1,72 @@
 package controllers
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/sKush-1/mongo-golang/models"
-	"gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type UserController struct {
-	session *mgo.Session
+	client *mongo.Client
 }
 
-func NewUserController(s *mgo.Session) *UserController {
-	return &UserController{s}
+func NewUserController(client *mongo.Client) *UserController {
+	return &UserController{client}
 }
 
 func (uc UserController) GetUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	id := p.ByName("id")
 
-	if !bson.IsObjectIdHex(id) {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
-	oid := bson.ObjectIdHex(id)
-	u := models.User{}
+	var u models.User
+	collection := uc.client.Database("mongo-golang").Collection("users")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-	err := uc.session.DB("mongo-golang").C("users").FindId(oid).One(&u)
+	err = collection.FindOne(ctx, bson.M{"_id": oid}).Decode(&u)
 	if err != nil {
 		w.WriteHeader(404)
 		return
 	}
 
 	uj, err := json.Marshal(u)
-
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	w.Header().Set("Content-Type", "appliction/json")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "%s\n", uj)
 }
 
 func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	u := models.User{}
+	var u models.User
 	json.NewDecoder(r.Body).Decode(&u)
-	u.Id = bson.NewObjectId()
+	u.Id = primitive.NewObjectID()
 
-	uc.session.DB("mongo-golang").C("users").Insert(u)
+	collection := uc.client.Database("mongo-golang").Collection("users")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_, err := collection.InsertOne(ctx, u)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Error creating user: %v", err)
+		return
+	}
 
 	uj, err := json.Marshal(u)
 	if err != nil {
@@ -67,17 +81,101 @@ func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request, _ ht
 func (uc UserController) DeleteUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	id := p.ByName("id")
 
-	if !bson.IsObjectIdHex(id) {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
 		w.WriteHeader(404)
 		return
 	}
 
-	oid := bson.ObjectIdHex(id)
-	if err := uc.session.DB("mongo-golang").C("users").RemoveId(oid); err != nil {
+	collection := uc.client.Database("mongo-golang").Collection("users")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	result, err := collection.DeleteOne(ctx, bson.M{"_id": oid})
+	if err != nil || result.DeletedCount == 0 {
 		w.WriteHeader(404)
 		return
 	}
 
 	w.WriteHeader(http.StatusNoContent)
 	fmt.Fprintf(w, "Deleted user with id %s\n", oid)
+}
+
+func (uc UserController) UpdateUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := p.ByName("id")
+
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Invalid user ID format")
+		return
+	}
+
+	var updateData models.User
+	err = json.NewDecoder(r.Body).Decode(&updateData)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Invalid JSON data")
+		return
+	}
+
+	collection := uc.client.Database("mongo-golang").Collection("users")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Create update document (only update non-empty fields)
+	update := bson.M{}
+	if updateData.Name != "" {
+		update["name"] = updateData.Name
+	}
+	if updateData.Gender != "" {
+		update["gender"] = updateData.Gender
+	}
+	if updateData.Age != 0 {
+		update["age"] = updateData.Age
+	}
+
+	if len(update) == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "No valid fields to update")
+		return
+	}
+
+	result, err := collection.UpdateOne(
+		ctx,
+		bson.M{"_id": oid},
+		bson.M{"$set": update},
+	)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Error updating user: %v", err)
+		return
+	}
+
+	if result.MatchedCount == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "User not found")
+		return
+	}
+
+	// Fetch and return the updated user
+	var updatedUser models.User
+	err = collection.FindOne(ctx, bson.M{"_id": oid}).Decode(&updatedUser)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Error fetching updated user: %v", err)
+		return
+	}
+
+	uj, err := json.Marshal(updatedUser)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Error marshaling response: %v", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s\n", uj)
 }
--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/julienschmidt/httprouter"
 	"github.com/sKush-1/mongo-golang/controllers"
-	"gopkg.in/mgo.v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
@@ -18,24 +21,49 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
+	client := getMongoClient()
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
 	r := httprouter.New()
-	uc := controllers.NewUserController(getSession())
+	uc := controllers.NewUserController(client)
 	r.GET("/user/:id", uc.GetUser)
 	r.POST("/user", uc.CreateUser)
+	r.PUT("/user/:id", uc.UpdateUser)
 	r.DELETE("/user/:id", uc.DeleteUser)
 
+	log.Println("Server starting on localhost:8080")
 	http.ListenAndServe("localhost:8080", r)
 }
 
-func getSession() *mgo.Session {
+func getMongoClient() *mongo.Client {
 	mongoURL := os.Getenv("MONGODB_URL")
 	if mongoURL == "" {
 		log.Fatal("MONGODB_URL environment variable is not set")
 	}
 
-	s, err := mgo.Dial(mongoURL)
+	// Set client options
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	
+	// Create context with timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
 		log.Fatalf("Failed to connect to MongoDB: %v", err)
 	}
-	return s
+
+	// Test the connection
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatalf("Failed to ping MongoDB: %v", err)
+	}
+
+	log.Println("Successfully connected to MongoDB!")
+	return client
 }
